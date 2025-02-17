57c859d0c859b732b46e4aa7e88fdfee8481bec3
bigballyguy214 <159861216+bigballyguy21@users.noreply.github.com>
2025-02-16 19:52:30 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //Create object for subclass pikachu
-        Pikachu pikachu = new Pikachu(5);
+        // Create object for subclass Pikachu
+        Pikachu playersPikachu = new Pikachu(5); // Fixed: Use playersPikachu directly
 
-        //Welcome Trainer
-        System.out.println("Welcome trainer to Codingmon! Your goal is to beat three pokemon good luck!");
+        // Welcome Trainer
+        System.out.println("Welcome trainer to Codingmon! Your goal is to beat three pokemon. Good luck!");
         System.out.println("Your pokemon: ");
-        //Show Pikachu's information
-        Pikachu playersPikachu = new Pikachu(5);
-        pikachu.displayInfo();
 
-        //asks player if they want to enter forest route to start their journey
+        // Show Pikachu's information
+        playersPikachu.displayInfo(); // Fixed: Use playersPikachu instead of pikachu
+
+        // Ask player if they want to enter forest route to start their journey
         System.out.println("Do you want to enter a route? (yes/no): ");
         String journeyBegins = scanner.nextLine();
 
@@ -22,23 +22,20 @@ public class Main {
             System.out.println("You are walking into the forest...");
             System.out.println("A wild Charmander appears!");
 
-            //create wild charmander
+            // Create wild Charmander
             Charmander wildCharmander = new Charmander(5);
             wildCharmander.displayInfo();
             wildCharmander.battleCry();
 
-            //create instance for battle between the players pokemon and the wild charmander
+            // Create instance for battle between the player's pokemon and the wild Charmander
             Battle battleInstance = new Battle();
 
-            //start battle
+            // Start battle
             battleInstance.battle(playersPikachu, wildCharmander);
 
         } else {
-            System.out.println("You decided not to start your journey and now you spend the rest of your life flipping" +
-                    " patties");
+            System.out.println("You decided not to start your journey and now you spend the rest of your life flipping patties.");
         }
         scanner.close();
     }
-
-
 }
