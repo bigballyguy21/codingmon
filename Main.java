bb0779cebdf248a8e5e7c6a338f6a319cf24a498
bigballyguy214 <159861216+bigballyguy21@users.noreply.github.com>
2025-02-16 22:34:36 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,64 +1,44 @@
-import java.util.Random;
-import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        // Ensure Pikachu and Charmander are recognized properly.
-        Codingmon playerCodingmon = new Pikachu();  // Using the Pikachu constructor
-        Codingmon wildCodingmon = new Charmander();  // Using the Charmander constructor
+        //Create object for subclass pikachu
+        Pikachu pikachu = new Pikachu(5);
 
-        // Battle loop
-        while (playerCodingmon.getHp() > 0 && wildCodingmon.getHp() > 0) {
-            System.out.println(playerCodingmon.getName() + "'s turn, Choose a move by entering 1, 2, or 3.");
-            int choice = input.nextInt();
-            int selfAttackChance = rand.nextInt(100);
-            int damageamount = 0;
+        //Welcome Trainer
+        System.out.println("Welcome trainer to Codingmon! Your goal is to beat three pokemon good luck!");
+        System.out.println("Your pokemon: ");
+        //Show Pikachu's information
+        Pikachu playersPikachu = new Pikachu(5);
+        pikachu.displayInfo();
 
-            // Player's attack logic
-            if (choice == 1 && selfAttackChance < 25) {
-                damageamount = 100;
-                System.out.println(wildCodingmon.getName() + " was hit for 100!");
-            } else if (choice == 2 && selfAttackChance < 75) {
-                damageamount = 50;
-                System.out.println(wildCodingmon.getName() + " was hit for 50!");
-            } else if (choice == 3 && selfAttackChance < 50) {
-                damageamount = 75;
-                System.out.println(wildCodingmon.getName() + " was hit for 75!");
-            } else {
-                System.out.println("You missed your attack! :(");
-            }
+        //asks player if they want to enter forest route to start their journey
+        System.out.println("Do you want to enter a route? (yes/no): ");
+        String journeyBegins = scanner.nextLine();
 
-            wildCodingmon.setHp(wildCodingmon.getHp() - damageamount);
+        if (journeyBegins.equalsIgnoreCase("yes")) {
+            System.out.println("You are walking into the forest...");
+            System.out.println("A wild Charmander appears!");
 
-            if (wildCodingmon.getHp() <= 0) {
-                System.out.println(wildCodingmon.getName() + " got CLAPPED!");
-                break;
-            }
+            //create wild charmander
+            Charmander wildCharmander = new Charmander(5);
+            wildCharmander.displayInfo();
+            wildCharmander.battleCry();
 
-            // Wild Codingmon attacks back
-            int enemyAttackChance = rand.nextInt(100);
-            if (enemyAttackChance < 50) {
-                int enemyDamage = rand.nextInt(50) + 50;
-                System.out.println(playerCodingmon.getName() + " was hit for " + enemyDamage);
-                playerCodingmon.setHp(playerCodingmon.getHp() - enemyDamage);
-            } else {
-                System.out.println(wildCodingmon.getName() + " missed its attack!");
-            }
+            //create instance for battle between the players pokemon and the wild charmander
+            Battle battleInstance = new Battle();
 
-            if (playerCodingmon.getHp() <= 0) {
-                System.out.println(playerCodingmon.getName() + " got CLAPPED!");
-                break;
-            }
+            //start battle
+            battleInstance.battle(playersPikachu, wildCharmander);
 
-            // Battle cry at the end of the turn
-            playerCodingmon.battleCry();
-            wildCodingmon.battleCry();
+        } else {
+            System.out.println("You decided not to start your journey and now you spend the rest of your life flipping" +
+                    " patties");
         }
-
-        input.close();
+        scanner.close();
     }
+
+
 }
+//Benito
